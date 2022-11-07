97473aef942a640953cead5da69af8c8c1b89764
Ivan Pokusaev <ivanpoku1762022@outlook.com>
2022-11-07 00:05:37 -0500

    game

--- adventureGame/Form1.cs
+++ adventureGame/Form1.cs
@@ -1,5 +1,14 @@
-using System.Runtime.Versioning;
-using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
 
 namespace adventureGame
 {
